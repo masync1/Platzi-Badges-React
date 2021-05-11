e100e6c7dcf9081f525f830e13bf736f1839a118
Juan manuel <juan.manuel.masync@protonmail.com>
2021-05-10 22:24:30 -0500

    HEAD desacoplada en origin/1.ReactDOM.render style badge done

--- src/index.js
+++ src/index.js
@@ -8,9 +8,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+import BadgeNew from './pages/BadgeNew'
+
+import './global.css'
+import 'bootstrap/dist/css/bootstrap.css';
+// const element = <h1>Hello, Platzi Badges!</h1>;
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(<BadgeNew/>
+    , container);
+
+    // <Badge 
+    // firstName="Juan Manuel"
+    // secondName="Patiño Valencia"
+    // jobTitle="TI Especialist"
+    // account="Masync1"
+    // avatarURL="https://cdn.wallpapersafari.com/93/75/jb2pwy.png"/>
