f58c3d09451c3e2931c7d267225cd7676096ce03
Juan manuel <juan.manuel.masync@protonmail.com>
2021-05-17 19:26:29 -0500

    En la rama platziBadges2.0 Build

---
+++ build/precache-manifest.d4948d323096898b03c2342150bad861.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c23a1c16aa00e155e20b",
+    "url": "/static/css/main.3cad410b.chunk.css"
+  },
+  {
+    "revision": "c23a1c16aa00e155e20b",
+    "url": "/static/js/main.c23a1c16.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "0d5aa31db74eeab5a6c7",
+    "url": "/static/css/2.25c039bf.chunk.css"
+  },
+  {
+    "revision": "0d5aa31db74eeab5a6c7",
+    "url": "/static/js/2.0d5aa31d.chunk.js"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "23baaf04d0c43a851a3529952e7e8399",
+    "url": "/index.html"
+  }
+];
